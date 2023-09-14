848db81f610f8f681d582f09df8e02fb42541c97
anuragrajanp <anuragrajanp@gmail.com>
2023-09-14 16:29:37 +0530

    auth using Clerk

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,6 +1,8 @@
-import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import { ClerkProvider } from "@clerk/nextjs";
+import { dark } from "@clerk/themes";
+import "../globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -11,11 +13,10 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
 	return (
-		<html lang="en">
-			<head>
-				<link rel="icon" type="image/x-icon" href="/threads.png" sizes="any" />
-			</head>
-			<body className={inter.className}>{children}</body>
-		</html>
+		<ClerkProvider appearance={{ baseTheme: dark }}>
+			<html lang="en">
+				<body className={`${inter.className} bg-dark-1`}>{children}</body>
+			</html>
+		</ClerkProvider>
 	);
 }
--- app/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function Home() {
-	return (
-		<main>
-			<h1 className="text-center pt-[50vh]">Threads</h1>
-		</main>
-	);
-}
