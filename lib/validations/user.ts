4f8f0bba74d049676dfa0c255b3f9a92870d09ea
anuragrajanp <anuragrajanp@gmail.com>
2023-09-30 20:19:40 +0530

    recent thread first in profile + redirect(/) after logout + make bio not required

--- lib/validations/user.ts
+++ lib/validations/user.ts
@@ -3,6 +3,6 @@ import * as z from "zod";
 export const UserValidation = z.object({
 	profile_photo: z.string().url().nonempty(),
 	name: z.string().min(3, { message: "Minimum 3 characters." }).max(30, { message: "Maximum 30 caracters." }),
-	username: z.string().min(3, { message: "Minimum 3 characters." }).max(30, { message: "Maximum 30 caracters." }),
-	bio: z.string().min(3, { message: "Minimum 3 characters." }).max(1000, { message: "Maximum 1000 caracters." }),
+	username: z.string().min(3, { message: "Minimum 3 characters." }).max(25, { message: "Maximum 25 caracters." }),
+	bio: z.string().max(1000, { message: "Maximum 1000 caracters." }),
 });
